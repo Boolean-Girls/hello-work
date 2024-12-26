710504b1932fffb6a419414afd9f6d4f48b7b660
Choi-Young-Eun <cye8891@naver.com>
2024-12-26 20:03:24 +0900

    init project

---
+++ hello-work/src/main/java/com/choiye/hello_work/HelloWorkApplication.java
@@ -0,0 +1,13 @@
+package com.choiye.hello_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorkApplication.class, args);
+	}
+
+}
